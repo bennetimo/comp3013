d0cf45ec479be59c78a9ac0fb3a758b0479a9a5a
bennetimo <bennetimo@530d280e-011e-11df-9af4-1984060cff7e>
2010-03-04 16:48:25 +0000

    -Updated CSS design -Updated header and footer to include quick links

--- dev/system/application/views/main/main_view.php
+++ dev/system/application/views/main/main_view.php
@@ -1,49 +1,65 @@
 
-<div id="login_box">
+<h3>Welcome.</h3>
+<p>
+Musique is an online streaming media player allowing you to listen to 
+all your favourite music, and share it easily with your friends.
 
-<? if($userid == ""): ?>
-
-<h3>Login</h3>
-
-<form id="login_form" action="<?=site_url('usermanager/login')?>" method="POST">
-
-	<input type="text" id="login_email" name="login_email"></input>
-	<input type="password" id="login_password" name="login_password"></input>
-	<input type="submit" value="login"></input>
-	
-	<div id="login_error" style="display: none;"></div>
-	
-</form>
-
-<a href="<?=site_url('register')?>">register</a>
-
-<? else: ?>
-
-<h3>Logout <?=$userid?></h3>
-
-<a href="<?=site_url('usermanager/logout')?>">logout</a>
-
-<? endif; ?>
-
-</div>
+Get started today to see what you've been missing
+</p>
 
 <div id="search_box">
+	<h3>Find Music.</h3>
+	<form action="<?=site_url('trackmanager/search')?>" method="POST">
+		<input type="text" name="search"></input>
+		<input type="submit" value="search"></input>
+	</form>	
+</div>
 
-<h3>Search</h3>
-
-<form action="<?=site_url('trackmanager/search')?>" method="POST">
-	
-	<input type="text" name="search"></input>
-	<input type="submit" value="search"></input>
+<div class="content_box">
+	<div id="login_box">
+		<? if($userid == ""): ?>
+		<h3>Login</h3>
 		
-</form>
+		<form id="login_form" action="<?=site_url('usermanager/login')?>" method="POST">
 
-<div id="search_results">
-<table id="search_results_table">
-</table>
+			<input type="text" id="login_email" name="login_email"></input>
+			<input type="password" id="login_password" name="login_password"></input>
+			<input type="submit" value="login"></input>
+	
+			<div id="login_error" style="display: none;"></div>
+	
+		</form>
+		
+		<a href="<?=site_url('register')?>">register</a>
+		<? else: ?>
+		<h3>Logout</h3>
+		<a href="<?=site_url('usermanager/logout')?>">logout</a>
+		<? endif; ?>
+	</div>
+	
+	<div id="register_box">
+		<? if($userid == ""): ?>
+		<h3>Register</h3>
+		
+		<form action="<?=site_url('usermanager/login')?>" method="POST">
+			<input type="text" id="login_email" name="login_email"></input>
+			<input type="password" id="login_password" name="login_password"></input>
+			<input type="submit" value="login"></input>
+		</form>
+		
+		<a href="<?=site_url('register')?>">register</a>
+		<? else: ?>
+		<h3>Logout</h3>
+		<a href="<?=site_url('usermanager/logout')?>">logout</a>
+		<? endif; ?>
+	</div>
 </div>
-
-</div>
+	
+	<div id="search_results">
+		<table id="search_results_table">
+		</table>
+	</div>
+	
 
 <script>
 
