c665887a3470c07b6e0c23cbb55f428c3554322e
giacomolg <giacomolg@530d280e-011e-11df-9af4-1984060cff7e>
2010-03-13 16:43:36 +0000

    fixed error handling

--- dev/system/application/controllers/trackmanager.php
+++ dev/system/application/controllers/trackmanager.php
@@ -6,38 +6,44 @@ class TrackManager extends Controller {
 	{
 		parent::Controller();
 	}
-	
+
 	function index()
 	{
 		echo "TrackManager Controller...";
 	}
-	
+
 	function search()
 	{
 		$searchBy = $_POST["search_by"];
 		$term = $_POST["search_term"];
 		$userid = $this->session->userdata('userid');
 		$tracks = array();
-		
+
 		$this->load->static_model("Track");
-		
-		if ($searchBy == "name") {
-			$tracks = Track::searchTrackName($term, $userid);
+
+		try{
+			if ($searchBy == "name") {
+				$tracks = Track::searchTrackName($term, $userid);
+			}
+			else if ($searchBy == "genre") {
+				$tracks = Track::searchByGenre($term, $userid);
+			}
+			else {
+				echo json_encode(array("error" => "The search criteria is not recognized"));
+				return;
+			}
 		}
-		else if ($searchBy == "genre") {
-			$tracks = Track::searchByGenre($term, $userid);
+		catch(Exception $e) {
+				echo json_encode(array("error" => $e->getMessage()));
+        return;
 		}
-		else {
-			echo json_encode(array("error" => TRUE));
-		}
-		
 		$newTracks = array();
-		
+
 		foreach ($tracks as $track) {
-			
+				
 			$newTracks[] = $track->toArray();
 		}
-		
+
 		echo json_encode($newTracks);
 	}
 }
