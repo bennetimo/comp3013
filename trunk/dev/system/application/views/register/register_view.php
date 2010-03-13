b8a045be9512b7562f02c2c9150f5fd0bc2e47be
bennetimo <bennetimo@530d280e-011e-11df-9af4-1984060cff7e>
2010-03-13 19:09:45 +0000

    -Cosmetic changes to register view

--- trunk/dev/system/application/views/register/register_view.php
+++ trunk/dev/system/application/views/register/register_view.php
@@ -1,32 +1,41 @@
-<form id="reg_form" action="<?=site_url('usermanager/register')?>" method="POST">
 
-	<table>
-		<tr>
-			<td>First Name:</td>
-			<td><input type="text" name="reg_first_name"></input></td>
-		</tr>
-		<tr>
-			<td>last Name:</td>
-			<td><input type="text" name="reg_last_name"></input></td>
-		</tr>
-		<tr>
-			<td>Email:</td>
-			<td><input type="text" name="reg_email"></input></td>
-		</tr>
-		<tr>
-			<td>Password:</td>
-			<td><input type="password" name="reg_password"></input></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td><input type="submit" value="register"></input></td>
-		</tr>
-		<tr>
-			<td colspan="2"><div id="reg_error" style="display: none;"></div></td>
-		</tr>
-	</table>
+<div class="content_box">
 
-</form>
+	<p>So you would like to get started with a Musique account? No problem!</p>
+	<p>Just fill out your details below and you can be using our service within minutes.</p>
+
+	<form id="reg_form" action="<?=site_url('usermanager/register')?>" method="POST">
+	
+		<table>
+			<tr>
+				<td>First Name:</td>
+				<td><input type="text" name="reg_first_name"></input></td>
+			</tr>
+			<tr>
+				<td>last Name:</td>
+				<td><input type="text" name="reg_last_name"></input></td>
+			</tr>
+			<tr>
+				<td>Email:</td>
+				<td><input type="text" name="reg_email"></input></td>
+			</tr>
+			<tr>
+				<td>Password:</td>
+				<td><input type="password" name="reg_password"></input></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" class="normal_button" value="register"></input></td>
+			</tr>
+			<tr>
+				<td colspan="2"><div id="reg_error" style="display: none;"></div></td>
+			</tr>
+		</table>
+		
+		<p>Note. Please remember to use a real email address, as we will send an activation request to it!</p>
+	
+	</form>
+</div>
 
 <script>
 
