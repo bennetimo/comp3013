bed11090e68cbbb8f65387c51758bdd5f313ea6f
giacomolg <giacomolg@530d280e-011e-11df-9af4-1984060cff7e>
2010-03-13 22:21:09 +0000

    updated swfobject to newest version

--- trunk/dev/system/application/views/common/header.php
+++ trunk/dev/system/application/views/common/header.php
@@ -4,12 +4,12 @@
 	<title>Musique</title>
 	<link type="text/css" rel="stylesheet" href="<?=base_url()?>system/application/css/main.css"></link>
 	<link type="text/css" rel="stylesheet" href="<?=base_url()?>system/application/css/dynamicList.css"></link>
+	<script src="<?=base_url()?>system/application/js/swfobject.js" type="text/javascript"></script>
 	<script src="<?=base_url()?>system/application/js/jquery.js" type="text/javascript"></script>
 	<script src="<?=base_url()?>system/application/js/jquery-ui.js" type="text/javascript"></script>
 	<script src="<?=base_url()?>system/application/js/jquery-template.js" type="text/javascript"></script>
 	<script src="<?=base_url()?>system/application/js/utils.js" type="text/javascript"></script>
 	<script src="<?=base_url()?>system/application/js/player.js" type="text/javascript"></script>
-	<script src="<?=base_url()?>system/application/js/swfobject.js" type="text/javascript"></script>
 </head>
 
 <body>
